b0e34853ea4ec20eace95111dad82095d1e1a5c0
android <trevd1234@gmail.com>
2014-01-02 21:36:07 +0000

    add file extraction add checks for mount point and directory existing

--- recovery/recovery_updater.c
+++ recovery/recovery_updater.c
@@ -17,107 +17,142 @@
 #include <sys/mount.h>
 #include <sys/stat.h>
 #include <sys/mman.h>
+#include <unistd.h>
 #include <fcntl.h>
-
+#include "minzip/Zip.h"
+#include "updater/updater.h"
+#include "mtdutils/mounts.h"
 #include "edify/expr.h"
 
+struct MountedVolume {
+    const char *device;
+    const char *mount_point;
+    const char *filesystem;
+    const char *flags;
+};
 
 #define MOUNT_LOCATION "/dev/block/mmcblk0p1"
-#define RAWFS_MOUNT_POINT "/tmp/rawfs"
-#define RAWFS_CUSTOM_FILENAME RAWFS_MOUNT_POINT"custom"
+#define RAWFS_MOUNT_POINT "/rawfs"
+#define RAWFS_CUSTOM_FILENAME RAWFS_MOUNT_POINT"/custom"
 
 static inline int return_error(char* message) {
 	printf(message);
 	return -1 ;
 }
 
-static inline int write_custom_file(char* filename)
-{
-	struct stat source,dest;
-	int wrote = 0 ; 
+static inline int mount_rawfs(){
 	
-   // Get the boot image source file size 
-   if (stat(filename, &source) == -1) 
-		return return_error("Cannot Stat source File");
-		   
-	// Get the destination file size
-	if (stat( RAWFS_CUSTOM_FILENAME , &dest) == -1) 
-		  return return_error("Cannot Stat destination File");
+	printf("mount_rawfs is called\n"); 
 	
-	if (source.st_size > dest.st_size )
-		   return return_error("Source File is Too Big!");
-	
-	int fd  = open(filename,O_RDONLY);
-	if (fd<0){
-		return return_error("Error:Cannot Open source file!!\n");
-		
-	}
-	
-	// allocate a buffer which fits the destination
-	char* buffer = calloc(dest.st_size,1);
-	if(!buffer){
-		fprintf(stderr,"Error: Allocating memory!!\n");
-		goto cleanup_and_die;
-	}
-	
-	// mmap the full source file
-	buffer = mmap(NULL, source.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
-	if (buffer == MAP_FAILED) {
-		fprintf(stderr,"Error: loading file!!\n"	);
-		goto cleanup_and_die;
-	}
-	close(fd);
-	
-	if ((fd = open(RAWFS_CUSTOM_FILENAME,  O_WRONLY )) < 0 ){
-		fprintf(stderr,"Error:Cannot Open Output File %s!!\n",RAWFS_CUSTOM_FILENAME);
-		goto cleanup_and_die;		
-	}
-	wrote = write(fd,buffer,dest.st_size);
-		
-cleanup_and_die:
-	close(fd);
-	if(buffer) munmap(buffer, dest.st_size);
-	return wrote ; 
-	
+	errno = 0;
+	if( mkdir(RAWFS_MOUNT_POINT,0777) < 0){
+		if (errno != EEXIST ){
+			printf("mount_rawfs is failed\n"); 
+			return -1;
+		}
+	}	
+	if (mount(MOUNT_LOCATION, RAWFS_MOUNT_POINT, "rawfs",MS_NOATIME | MS_NODEV | MS_NODIRATIME, "") < 0) {
+            printf("%s: failed to mount %s: %s\n",MOUNT_LOCATION, RAWFS_MOUNT_POINT, strerror(errno));
+            rmdir(RAWFS_MOUNT_POINT);
+			return -1;
+	} 
+	printf("mounted: %s @ %s\n",MOUNT_LOCATION, RAWFS_MOUNT_POINT);
+	return 0 ; 
 }
 
-// write_sde_image(sde_boot_image)
-Value* WriteSDEImage(const char* name, State* state, int argc, Expr* argv[]) {
 
-    char* sdeImage;
-    int wrote = 0;
+// write_sde_image(sde_boot_image)
+Value* WriteSDEImageFn(const char* name, State* state, int argc, Expr* argv[]) {
+
+   printf("WriteSDEImageFn is called\n"); 
+	char* sdeImage;
+	 struct stat dest;
+    int success = 0;
 
     if (argc != 1)
         return ErrorAbort(state, "%s() expects 1 arg, got %d", name, argc);
 
-    if (ReadArgs(state, argv, 1, &sdeImage) != 0) {
+    if (ReadArgs(state, argv, 1, &sdeImage) != 0)
         return NULL;
-    }
-	mkdir(RAWFS_MOUNT_POINT, 0755);
 	
-	 if (mount(MOUNT_LOCATION, RAWFS_MOUNT_POINT, "rawfs",
-                  MS_NOATIME | MS_NODEV | MS_NODIRATIME, "") < 0) {
-            printf("%s: failed to mount %s: %s\n",
-                   MOUNT_LOCATION, RAWFS_MOUNT_POINT, strerror(errno));
-            
-			goto done;
-	} 
+	// Umount any rawfs mounts
+	scan_mounted_volumes();
+   const MountedVolume* vol = find_mounted_volume_by_device(MOUNT_LOCATION);
+	if(vol){
+		printf("unmounting rawfs mounted at %s\n",vol->mount_point);
+		unmount_mounted_volume(vol);
+	}
+		
+	// Mount Rawfs at a mount temporary mount point
+	// TODO : Work out what is required to write to the 
+	// block device directly
+	if(mount_rawfs()<0) 
+		goto done;
+		
+	// Get the file size for /rawfs/custom
+	if (stat( RAWFS_CUSTOM_FILENAME , &dest) == -1) {
+		 printf("Cannot Stat destination File");
+		goto done;
 	
-	printf("mounted: %s @ %s\n",MOUNT_LOCATION, RAWFS_MOUNT_POINT);
-	  wrote =  write_custom_file(sdeImage);
-    if (wrote <= 0) {
-        printf("Failed to write sde image %s!\n",sdeImage);
-    } else {
-        printf("sde image written!\n");
-    }
-	 if(umount(RAWFS_MOUNT_POINT))
-		 rmdir(RAWFS_MOUNT_POINT);
+	}
+	// find the sdeImage - Normally called boot.img
+	ZipArchive* za = ((UpdaterInfo*)(state->cookie))->package_zip;
+	const ZipEntry* entry = mzFindZipEntry(za, sdeImage);
+	if (entry == NULL) {
+		printf("%s: no %s in package\n", name, sdeImage);
+		goto done;
+	}
+	long sde_size = mzGetZipEntryUncompLen(entry);
+	if(sde_size > dest.st_size){
+		printf("%s: %s is to large\n", name, sdeImage);
+		goto done;
+	}
+	// allocate a buffer which fits the destination
+	char* buffer = calloc(dest.st_size,1);
+	if(!buffer){
+		printf("Error: Allocating memory!!\n");
+		goto done;
+	}
+	int zipsuccess = mzExtractZipEntryToBuffer(za, entry,(unsigned char *)buffer);
+	// Write the output file
+	int fd  = open(RAWFS_CUSTOM_FILENAME,O_WRONLY);
+	if (fd<0){
+		printf("Error:Cannot Create Output File %s!!\n",sdeImage);
+		goto done2;		
+	}
 	
+	long bytes = write(fd,buffer,dest.st_size);
+	if ( bytes <= 0 ){
+		printf("Error:Cannot Create Output File %s!!\n",sdeImage);
+		goto done3;
+	}
+	
+	printf("%s written - %ld!!\n",sdeImage,bytes);
+	if(fsync(fd) == -1){
+		printf("fsync failed Error: %d %s!!\n",errno,strerror(errno));
+	}
+	// Umount our rawfs mount
+	int umount_result = umount2(RAWFS_MOUNT_POINT,MNT_DETACH);
+	if(umount_result == -1){
+		printf("unmount error %d %s\n",errno,strerror(errno));	
+	}
+	printf("unmounting rawfs mounted at %s result %d\n",RAWFS_MOUNT_POINT,umount_result);	
+	if(umount_result == 0 ) 
+		rmdir(RAWFS_MOUNT_POINT);
+		
+	success = 1 ; 
+    
+done3:
+	close(fd);
+done2:
+	free(buffer);
 done:
-    return StringValue(strdup(wrote > 0 ? "t" : ""));
+	 
+	 free(sdeImage);
+    return StringValue(strdup(success > 0 ? "t" : ""));
 }
 
 void Register_librecovery_updater_a80sboard() {
     printf("Register_librecovery_updater_a80sboard is called\n");
-    RegisterFunction("archos.write_sde_image", WriteSDEImage);
+    RegisterFunction("archos.write_sde_image", WriteSDEImageFn);
 }
