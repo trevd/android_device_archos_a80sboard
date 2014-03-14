7e3f5c3d642618c97cd6ee0dd8d7f0af2de9d4b8
android <trevd1234@gmail.com>
2014-03-14 10:35:02 +0000

    add initial parted partition checking

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -4,12 +4,11 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := eng
 LOCAL_C_INCLUDES += bootable/recovery \
-					bootable/recovery/mtdutils \
-                    external/libparted/include
+		    bootable/recovery/mtdutils \
+                    external/parted/include
 
 LOCAL_SRC_FILES := recovery_updater.c
-LOCAL_STATIC_LIBRARIES := libparted_static
-
+LOCAL_WHOLE_STATIC_LIBRARIES := libparted libparted-fs libgnulib libuuid
 # should match TARGET_RECOVERY_UPDATER_LIBS set in BoardConfig.mk
 LOCAL_MODULE := librecovery_updater_a80sboard
 
--- recovery/recovery_updater.c
+++ recovery/recovery_updater.c
@@ -26,6 +26,8 @@
 #include "mounts.h"
 #include "edify/expr.h"
 
+#define EOK 0
+
 struct MountedVolume {
     const char *device;
     const char *mount_point;
@@ -43,49 +45,49 @@ extern Value* DeleteFn(const char* name, State* state, int argc, Expr* argv[]) ;
 #define RAWFS_CUSTOM_FILENAME RAWFS_MOUNT_POINT"/custom"
 
 static inline int return_error(char* message) {
-	printf("%s",message);
-	return -1 ;
+        printf("%s",message);
+        return -1 ;
 }
 
 static inline int mount_rawfs(){
-	
-	printf("mount_rawfs is called\n"); 
-	
-	errno = 0;
-	if( mkdir(RAWFS_MOUNT_POINT,0777) < 0){
-		if (errno != EEXIST ){
-			printf("mount_rawfs is failed\n"); 
-			return -1;
-		}
-	}	
-	if (mount(MOUNT_LOCATION, RAWFS_MOUNT_POINT, "rawfs",MS_NOATIME | MS_NODEV | MS_NODIRATIME, "") < 0) {
+        
+        printf("mount_rawfs is called\n"); 
+        
+        errno = 0;
+        if( mkdir(RAWFS_MOUNT_POINT,0777) < 0){
+                if (errno != EEXIST ){
+                        printf("mount_rawfs is failed\n"); 
+                        return -1;
+                }
+        }       
+        if (mount(MOUNT_LOCATION, RAWFS_MOUNT_POINT, "rawfs",MS_NOATIME | MS_NODEV | MS_NODIRATIME, "") < 0) {
             printf("%s: failed to mount %s: %s\n",MOUNT_LOCATION, RAWFS_MOUNT_POINT, strerror(errno));
             rmdir(RAWFS_MOUNT_POINT);
-			return -1;
-	} 
-	printf("mounted: %s @ %s\n",MOUNT_LOCATION, RAWFS_MOUNT_POINT);
-	return 0 ; 
+                        return -1;
+        } 
+        printf("mounted: %s @ %s\n",MOUNT_LOCATION, RAWFS_MOUNT_POINT);
+        return 0 ; 
 }
 // MountAndWipeFn(fs_type, partition_type, location, mount_point)
 // archos.mount_and_wipe replaces the default format and mount commands
 // We mount the selected partiton then perfoming a recursive delete.
 // This is instead of formatting the partition.
 Value* MountAndWipeFn(const char* name, State* state, int argc, Expr* argv[]) {
-	
-	printf("MountAndWipeFn is called\n"); 
-	    char* result = NULL;
+        
+        printf("MountAndWipeFn is called\n"); 
+            char* result = NULL;
     if (argc != 4) {
         return ErrorAbort(state, "%s() expects 4 args, got %d", name, argc);
     }
-	char* fs_type;
-	char* partition_type;
-	char* location;
-	char* mount_point;
-	if (ReadArgs(state, argv, 4, &fs_type, &partition_type,
-				 &location, &mount_point) < 0) {
-		return NULL;
-	}
-	    if (strlen(fs_type) == 0) {
+        char* fs_type;
+        char* partition_type;
+        char* location;
+        char* mount_point;
+        if (ReadArgs(state, argv, 4, &fs_type, &partition_type,
+                                 &location, &mount_point) < 0) {
+                return NULL;
+        }
+            if (strlen(fs_type) == 0) {
         ErrorAbort(state, "fs_type argument to %s() can't be empty", name);
         goto done;
     }
@@ -109,26 +111,26 @@ Value* MountAndWipeFn(const char* name, State* state, int argc, Expr* argv[]) {
         selabel_lookup(sehandle, &secontext, mount_point, 0755);
         setfscreatecon(secontext);
     }
-	
-	Expr* ismounted_argv[] = { argv[3] }; //  mount_point
-	Value* ismountedfn = IsMountedFn("is_mounted",state,1, ismounted_argv );
-	Value* mountfn = NULL ;  
-	printf("MountAndWipeFn :IsMountedFn is called\n"); 
-	if((ismountedfn != NULL) && (strlen(ismountedfn->data) == 0)){
-		mountfn = MountFn("mount",state,argc,argv); 
-		printf("MountAndWipeFn :MountFn is called\n"); 
-		if((mountfn == NULL) || (strlen(mountfn->data) == 0)){
-			ErrorAbort(state,"Failed to mount %s\n",mount_point);
-			goto done2;
-		}
-		
-	}
-	result = mount_point;
+        
+        Expr* ismounted_argv[] = { argv[3] }; //  mount_point
+        Value* ismountedfn = IsMountedFn("is_mounted",state,1, ismounted_argv );
+        Value* mountfn = NULL ;  
+        printf("MountAndWipeFn :IsMountedFn is called\n"); 
+        if((ismountedfn != NULL) && (strlen(ismountedfn->data) == 0)){
+                mountfn = MountFn("mount",state,argc,argv); 
+                printf("MountAndWipeFn :MountFn is called\n"); 
+                if((mountfn == NULL) || (strlen(mountfn->data) == 0)){
+                        ErrorAbort(state,"Failed to mount %s\n",mount_point);
+                        goto done2;
+                }
+                
+        }
+        result = mount_point;
    Value* deletefn  = DeleteFn("delete_recursive",state,1, ismounted_argv );
-	
+        
 done2: 
-	if(mountfn)free(mountfn);
-	if(ismountedfn)free(ismountedfn);
+        if(mountfn)free(mountfn);
+        if(ismountedfn)free(ismountedfn);
 done:
 
     free(fs_type);
@@ -136,7 +138,7 @@ done:
     free(location);
     if (result != mount_point) free(mount_point);
     return StringValue(result);
-}	
+}       
 
 // write_sde_image(sde_boot_image)
 // Write the archos sde image to the custom file located in the rawfs partition
@@ -144,8 +146,8 @@ done:
 Value* WriteSDEImageFn(const char* name, State* state, int argc, Expr* argv[]) {
 
    printf("WriteSDEImageFn is called\n"); 
-	char* sdeImage;
-	 struct stat dest;
+        char* sdeImage;
+         struct stat dest;
     int success = 0;
 
     if (argc != 1)
@@ -153,110 +155,150 @@ Value* WriteSDEImageFn(const char* name, State* state, int argc, Expr* argv[]) {
 
     if (ReadArgs(state, argv, 1, &sdeImage) != 0)
         return NULL;
-	
-	// Umount any rawfs mounts
-	scan_mounted_volumes();
+        
+        // Umount any rawfs mounts
+        scan_mounted_volumes();
    const MountedVolume* vol = find_mounted_volume_by_device(MOUNT_LOCATION);
-	if(vol){
-		printf("unmounting rawfs mounted at %s\n",vol->mount_point);
-		unmount_mounted_volume(vol);
-	}
-		
-	// Mount Rawfs at a mount temporary mount point
-	// TODO : Work out what is required to write to the 
-	// block device directly
-	if(mount_rawfs()<0) 
-		goto done;
-		
-	// Get the file size for /rawfs/custom
-	if (stat( RAWFS_CUSTOM_FILENAME , &dest) == -1) {
-		 printf("Cannot Stat destination File");
-		goto done;
-	
-	}
-	// find the sdeImage - Normally called boot.img
-	ZipArchive* za = ((UpdaterInfo*)(state->cookie))->package_zip;
-	const ZipEntry* entry = mzFindZipEntry(za, sdeImage);
-	if (entry == NULL) {
-		printf("%s: no %s in package\n", name, sdeImage);
-		goto done;
-	}
-	long sde_size = mzGetZipEntryUncompLen(entry);
-	if(sde_size > dest.st_size){
-		printf("%s: %s is to large\n", name, sdeImage);
-		goto done;
-	}
-	// allocate a buffer which fits the destination
-	char* buffer = calloc(dest.st_size,1);
-	if(!buffer){
-		printf("Error: Allocating memory!!\n");
-		goto done;
-	}
-	int zipsuccess = mzExtractZipEntryToBuffer(za, entry,(unsigned char *)buffer);
-	// Write the output file
-	int fd  = open(RAWFS_CUSTOM_FILENAME,O_WRONLY);
-	if (fd<0){
-		printf("Error:Cannot Create Output File %s!!\n",sdeImage);
-		goto done2;		
-	}
-	
-	long bytes = write(fd,buffer,dest.st_size);
-	if ( bytes <= 0 ){
-		printf("Error:Cannot Create Output File %s!!\n",sdeImage);
-		goto done3;
-	}
-	
-	printf("%s written - %ld!!\n",sdeImage,bytes);
-	if(fsync(fd) == -1){
-		printf("fsync failed Error: %d %s!!\n",errno,strerror(errno));
-	}
-	// Umount our rawfs mount
-	int umount_result = umount2(RAWFS_MOUNT_POINT,MNT_DETACH);
-	if(umount_result == -1){
-		printf("unmount error %d %s\n",errno,strerror(errno));	
-	}
-	printf("unmounting rawfs mounted at %s result %d\n",RAWFS_MOUNT_POINT,umount_result);	
-	if(umount_result == 0 ) 
-		rmdir(RAWFS_MOUNT_POINT);
-		
-	success = 1 ; 
+        if(vol){
+                printf("unmounting rawfs mounted at %s\n",vol->mount_point);
+                unmount_mounted_volume(vol);
+        }
+                
+        // Mount Rawfs at a mount temporary mount point
+        // TODO : Work out what is required to write to the 
+        // block device directly
+        if(mount_rawfs()<0) 
+                goto done;
+                
+        // Get the file size for /rawfs/custom
+        if (stat( RAWFS_CUSTOM_FILENAME , &dest) == -1) {
+                 printf("Cannot Stat destination File");
+                goto done;
+        
+        }
+        // find the sdeImage - Normally called boot.img
+        ZipArchive* za = ((UpdaterInfo*)(state->cookie))->package_zip;
+        const ZipEntry* entry = mzFindZipEntry(za, sdeImage);
+        if (entry == NULL) {
+                printf("%s: no %s in package\n", name, sdeImage);
+                goto done;
+        }
+        long sde_size = mzGetZipEntryUncompLen(entry);
+        if(sde_size > dest.st_size){
+                printf("%s: %s is to large\n", name, sdeImage);
+                goto done;
+        }
+        // allocate a buffer which fits the destination
+        char* buffer = calloc(dest.st_size,1);
+        if(!buffer){
+                printf("Error: Allocating memory!!\n");
+                goto done;
+        }
+        int zipsuccess = mzExtractZipEntryToBuffer(za, entry,(unsigned char *)buffer);
+        // Write the output file
+        int fd  = open(RAWFS_CUSTOM_FILENAME,O_WRONLY);
+        if (fd<0){
+                printf("Error:Cannot Create Output File %s!!\n",sdeImage);
+                goto done2;             
+        }
+        
+        long bytes = write(fd,buffer,dest.st_size);
+        if ( bytes <= 0 ){
+                printf("Error:Cannot Create Output File %s!!\n",sdeImage);
+                goto done3;
+        }
+        
+        printf("%s written - %ld!!\n",sdeImage,bytes);
+        if(fsync(fd) == -1){
+                printf("fsync failed Error: %d %s!!\n",errno,strerror(errno));
+        }
+        // Umount our rawfs mount
+        int umount_result = umount2(RAWFS_MOUNT_POINT,MNT_DETACH);
+        if(umount_result == -1){
+                printf("unmount error %d %s\n",errno,strerror(errno));  
+        }
+        printf("unmounting rawfs mounted at %s result %d\n",RAWFS_MOUNT_POINT,umount_result);   
+        if(umount_result == 0 ) 
+                rmdir(RAWFS_MOUNT_POINT);
+                
+        success = 1 ; 
     
 done3:
-	close(fd);
+        close(fd);
 done2:
-	free(buffer);
+        free(buffer);
 done:
-	 
-	 free(sdeImage);
+         
+         free(sdeImage);
     return StringValue(strdup(success > 0 ? "t" : ""));
 }
-// write_sde_image(sde_boot_image)
-// Write the archos sde image to the custom file located in the rawfs partition
-// This is /dev/block/mmcblk0p1 which is normally mounted at /mnt/rawfs
+
 Value* SetupPartitionsFn(const char* name, State* state, int argc, Expr* argv[]) {
 
-   printf("CheckPartitionsFn is called\n"); 
-	char* blkdevice;
-	 struct stat blkdevice_stat;
-    int success = 0;
+        printf("SetupPartitionsFn is called\n"); 
+        char* blkdevice;
+        struct stat blkdevice_stat;
+        int success = 0;
 
-    if (argc != 1)
-        return ErrorAbort(state, "%s() expects 1 arg, got %d", name, argc);
+        if (argc != 1){
+                return ErrorAbort(state, "%s() expects 1 arg, got %d", name, argc);
+        }
 
-    if (ReadArgs(state, argv, 1, &blkdevice) != 0)
-        return NULL;
+        if (ReadArgs(state, argv, 1, &blkdevice) != 0){
+                return NULL;
+        }
+
+        // use parted to get the block device
+        if(strncmp("/dev/block/mmcblk",blkdevice,17) != EOK){
+                ErrorAbort(state, "unexpected block device path :%s\n",blkdevice);
+                goto abort_arg;
+        }
+               
+
+        PedDevice* device = ped_device_get (blkdevice);
+        if (device == NULL){
+                ErrorAbort(state, "Parted cannot probe block device :%s\n",blkdevice);
+                goto abort_device;
+        }
+        printf("device:%s\n",blkdevice);
+
+        char* size_string = ped_unit_format_byte(device, device->length * device->sector_size);
+        if(size_string == NULL ) {
+                ErrorAbort(state, "Parted cannot get device size\n");
+                goto abort_device;
+        }
+        printf("size_string:%s\n",size_string);
+        // Expect a size string length of 6 in the format of 0000MB otherwise we'll bail
+        int size_string_length = strnlen(size_string,6);
+        if(size_string_length != 6 ){
+                ErrorAbort(state, "Unexpected size string length %d\n",size_string_length);
+                goto abort_size;
+        }
+        printf("size_string_length:%d\n",size_string_length);
         
-     
-    // Check if we need to setup the new partition layout
-	if (stat( LAST_MOUNT_LOCATION , &blkdevice_stat) != -1) {
-		 printf("New Partition Layout Found");
-		success =1 ;
-		goto done;
-	
-	}
+        unsigned long size_value = strtoul(size_string,NULL,10);
+        if((size_value < 7000) || (size_value > 8000)){
+                ErrorAbort(state, "Unexpected size value %lu \n",size_value);
+                goto abort_size;
+
+        }
+        printf("size_value:%lu\n",size_value);
+        
+        
+        
+
+        
+
+
+abort_size:
+        if(size_string) free(size_string);
+abort_device:
+        if(device) free(device);
+abort_arg:
+        if(blkdevice) free(blkdevice);
 done:
 
-    return StringValue(strdup(success > 0 ? "t" : ""));
+        return StringValue(strdup(success > 0 ? "t" : ""));
 
 }
 void Register_librecovery_updater_a80sboard() {
